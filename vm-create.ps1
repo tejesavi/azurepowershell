4e8feaf349192c3767710b8beede6ed082d89147
Ubuntu <azureadmin@VMProj7.cxeqn1cgsb4ujh5haue4ccgkdc.ux.internal.cloudapp.net>
2024-11-17 04:59:20 +0000

    New VM lalalalala

---
+++ vm-create.ps1
@@ -0,0 +1,23 @@
+# Variables for common values
+$resourceGroup = "ProjPow"
+$location = "canadacentral"
+$vmName = "VMPWScript"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Create a virtual machine
+New-AzVM `
+  -ResourceGroupName $resourceGroup `
+  -Name $vmName `
+  -Location $location `
+  -ImageName "Ubuntu2204" `
+  -VirtualNetworkName "myVnet" `
+  -SubnetName "mySubnet" `
+  -SecurityGroupName "myNetworkSecurityGroup" `
+  -PublicIpAddressName "myPublicIp" `
+  -Credential $cred `
+
